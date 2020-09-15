379cf9b475f5ce16f4c86a2ba6eb1eede6b76052
Cherry_Dasha <marmysheva2000@yandex.ru>
2020-09-15 17:07:06 +0300

    when you press the button ,all words should appear.through the cycle for.

--- app/src/main/java/com/example/myapplication/FirstFragment.java
+++ app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -9,12 +9,14 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 public class FirstFragment extends Fragment {
+    int first = new Random().nextInt(4);
 
     @Override
     public View onCreateView(
@@ -34,9 +36,21 @@ public class FirstFragment extends Fragment {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                for (int a = 0; a <= 4; a++) {
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    textWord.setText(words[a]);
+                }
                 int randomInt = random.nextInt(4);
-                textWord.setText(words[randomInt]);
-                Log.d("Random Int", Integer.toString(randomInt));
+                while (first == randomInt) {
+                    randomInt = random.nextInt(4);
+                }
+                first = randomInt;
+                textWord.setText(words[first]);
+                Log.d("Random Int", Integer.toString(first));
             }
         });
     }
